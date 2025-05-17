1aa70cd9c87bcb95d1eff87b47844a63cfcb9d33
Alireza <alireza@Alirezas-MacBook-Pro.local>
2025-05-17 17:47:29 +0330

    admin panel update commit

--- accounts/models.py
+++ accounts/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import AbstractUser
 class Guest(AbstractUser):
     phone_number = models.CharField(max_length=15, null=True, blank=True)
     address = models.TextField(null=True, blank=True)
+    is_manager = models.BooleanField(default=False, verbose_name="manager")
     
     def __str__(self):
         return f"{self.username} , ({self.email})"
