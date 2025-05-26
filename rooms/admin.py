d70ad19d4ba32a93e9412c6f01099521b26ef8e3
Alireza <alireza@Alirezas-MacBook-Pro.local>
2025-05-26 19:05:49 +0330

    room_list and room_detail update

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Room
-# Register your models here.
+from .models import Room, RoomImage
 
-admin.site.register(Room)
+class RoomImageInline(admin.TabularInline):
+    model = RoomImage
+    extra = 1
+
+class RoomAdmin(admin.ModelAdmin):
+    inlines = [RoomImageInline]
+
+admin.site.register(Room, RoomAdmin)
